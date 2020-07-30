3f1947416f8107355f613bf797d228c2a02c618f
熊鸣 <625225813@qq.com>
2020-07-30 20:15:16 +0800

    把本地工程添加到git

--- git-test-project/src/git/FirstClass.java
+++ git-test-project/src/git/FirstClass.java
@@ -9,4 +9,11 @@ public class FirstClass {
         System.out.println("hello git");
         System.out.println("hello git");
     }
+
+    public void add(){
+        int a=1;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
+    }
 }
